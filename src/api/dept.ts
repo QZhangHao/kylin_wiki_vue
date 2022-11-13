3fa80f39a8791cf1bab9b80b91007e82a8df9032
Howe <8596007@qq.com>
2022-11-13 19:04:26 +0800

    add comment

--- src/api/dept.ts
+++ src/api/dept.ts
@@ -1,21 +1,31 @@
 import  request  from '@/utils/request';
 
+/*
+* 获取部门列表
+*/
 export function getDeptList(){
     return request('/api/Dept/GetList','get')
 }
 
+/*
+* 分页获取部分列表
+*/
 export function getListByPage(params){
     return request('/api/Dept/GetListByPage','get',params)
 }
 
+
+//创建部门
 export function createDept(params){
     return request('/api/Dept','post',params)
 }
 
+//更新部门
 export function updateDept(params){
     return request('/api/Dept','put',params)
 }
 
+//删除部分
 export function deleteDept(params){
     return request(['/api/Dept', params].join('/'),'delete')
 }
